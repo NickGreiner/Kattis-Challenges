73112be6cb0c30422449eef76447e60e6d6a782b
Nick Greiner <nicholas.greiner@hotmail.com>
2020-04-19 21:22:56 -0400

    IsItHalloween.com
    Full challenge description found at https://open.kattis.com/problems/isithalloween.
    
    Input
    The input consists of a single line containing a date of the format FEB 9, with the month and date separated by a single space.
    
    Output
    If the date is October 31 or December 25, output yup. Otherwise, output nope.

---
+++ IsItHalloween.py
@@ -0,0 +1,11 @@
+# Nick Greiner
+# https://github.com/NickGreiner
+
+# Python 3.7
+
+dateInput = input()
+
+if dateInput == "OCT 31" or dateInput == "DEC 25":
+    print("yup")
+else:
+    print("nope")
