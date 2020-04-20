5c98e89903d66c6fb6c5b37ab11a82a497c310ec
Nick Greiner <nicholas.greiner@hotmail.com>
2020-04-19 21:00:24 -0400

    Faktor
    Input
    First and only line of input will contain 2 integers, A (1 ≤ A ≤ 100), the number of articles you plan to publish and I (1 ≤ I ≤ 100), the impact factor the owners require.
    
    Output
    The first and only line of output should contain one integer, the minimal number of scientists you need to bribe.

---
+++ faktor.py
@@ -0,0 +1,10 @@
+# Nick Greiner
+# https://github.com/NickGreiner
+
+# Python 3.7
+
+articles, faktor = input().split()
+articles = int(articles)
+faktor = int(faktor)
+
+print((faktor * articles) - articles + 1)
